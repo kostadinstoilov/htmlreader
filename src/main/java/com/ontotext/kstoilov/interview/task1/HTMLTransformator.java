9ecfe95184d1729c549aa3de69ccf2437dfd9147
Kostadin Stoilov <kmstoilov@gmail.com>
2015-07-19 13:53:21 +0300

    ADD: initial maven project setup

---
+++ src/main/java/com/ontotext/kstoilov/interview/task1/HTMLTransformator.java
@@ -0,0 +1,14 @@
+package com.ontotext.kstoilov.interview.task1;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+import org.ccil.cowan.tagsoup.jaxp.SAXParserImpl;
+
+public class HTMLTransformator {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
